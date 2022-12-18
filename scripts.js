be98dcb4ca899dfc7f2902ae8dae895de48175db
Teky1 <officialteky@gmail.com>
2022-12-17 19:49:28 -0500

    updates

--- scripts.js
+++ scripts.js
@@ -1,9 +1,30 @@
-window.onscroll = function() {myFunction()};
+window.onscroll = function() {scrollUpdate()};
+let expanded = false;
 
-function myFunction() {
+const navLinks = document.querySelectorAll('.nav-item')
+const menuToggle = document.getElementById('navLinks')
+const bsCollapse = new bootstrap.Collapse(menuToggle, {toggle:false})
+navLinks.forEach((l) => {
+  l.addEventListener('click', () => {
+    if(expanded) {
+      bsCollapse.toggle();
+      expanded = !expanded;
+    }
+  });
+})
+
+function expandHamburger() {
+  expanded = !expanded;
+  if(expanded) {
+    document.getElementsByTagName("nav")[0].classList.remove("navbarTransparent");
+  }
+  scrollUpdate();
+}
+
+function scrollUpdate() {
   if (document.documentElement.scrollTop > 50) {
     document.getElementsByTagName("nav")[0].classList.remove("navbarTransparent");
-  } else {
+  } else if(!expanded){
     document.getElementsByTagName("nav")[0].classList.add("navbarTransparent");
   }
 }
